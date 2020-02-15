2e1856ff7272f660a17bda4f8764ad87457498fc
darisjerez <56648815+darisjerez@users.noreply.github.com>
2020-02-15 12:37:48 -0400

    initial commit, includes base front end project and folder for backend and dependencies

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import { jasmine } from '../../node_modules/jasmine';
 
 describe('workspace-project App', () => {
   let page: AppPage;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+//Materialize components imports
+import { MzCardModule } from 'ngx-materialize';
+
+
+
 import { AppComponent } from './app.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactmeComponent } from './components/contactme/contactme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    ContactmeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserAnimationsModule,
+    MzCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
